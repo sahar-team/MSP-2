b7ed68ca40e0638a5c156d982d682fd41be9961a
Sofian Lechani <slechani@gmail.com>
2020-07-16 11:41:30 +0200

    design

--- main.py
+++ main.py
@@ -11,33 +11,12 @@ from azure.cognitiveservices.vision.contentmoderator import ContentModeratorClie
 import azure.cognitiveservices.vision.contentmoderator.models
 from msrest.authentication import CognitiveServicesCredentials
 app = Flask(__name__)
-# CONTENT_MODERATOR_ENDPOINT = os.environ.get("CONTENT_MODERATOR_ENDPOINT")
-# subscription_key = os.environ.get("CONTENT_MODERATOR_SUBSCRIPTION_KEY")
-# client = ContentModeratorClient(
-# endpoint=CONTENT_MODERATOR_ENDPOINT,
-#     credentials=CognitiveServicesCredentials(subscription_key)
-# )
 
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
-# def text_moderation(path):
-#     with open(path, "rb") as text_fd:
-#         screen = client.text_moderation.screen_text(
-#             text_content_type="text/plain",
-#             text_content=text_fd,
-#             language="fra",
-#             autocorrect=True,
-#             pii=True
-#         )
-#     try:
-#         pprint(len(screen.as_dict()['terms']))
-#         to_moderate = True
-#     except KeyError:
-#         to_moderate = False
-#     return to_moderate
 
 @app.route('/comment/new', methods=['POST'])
 def add_comment():
@@ -50,30 +29,20 @@ def add_comment():
         data = {'ContentValue': comment}
         response = requests.post(url, headers=headers, data=data)
         if response.json()["Terms"] is not None:        
-            return Response("méchant")
+            return Response("Votre commentaire a été modéré.")
         else:
-            return Response("gentil")    
-          
-        # return render_template("/index.html")
-        # moderate = text_moderation("test")
-        # if moderate is True:            
-        #     return Response("méchant")
-        # else:
-        #     return Response("gentil")    
+            return Response("Votre commentaire a été enregistré.")    
 
     
     req_data = request.get_json()
     comment = req_data['comment']
 
-    # Add comment to the list
     res_data = helper.add_to_list(comment)
 
-    # Return error if comment not added
     if res_data is None:
         response = Response("{'error': 'comment not added - " + comment + "'}", status=400 , mimetype='application/json')
         return response
 
-    # Return response
     response = Response(json.dumps(res_data), mimetype='application/json')
 
     return render_template("index.html")
